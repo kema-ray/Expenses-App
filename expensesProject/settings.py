5bef7b8803c4b614320350c0ad66e6546907529d
kema-ray <rachelkemuma99@gmail.com>
2022-10-07 15:51:42 +0300

    Create a database and configure it in settings

--- expensesProject/settings.py
+++ expensesProject/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'expensesProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'expenses',
+        'USER': 'rachel',
+        'PASSWORD': 'hotspurs'
     }
 }
 
